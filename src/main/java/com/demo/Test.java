4b16f04bdf474e23de14769a26751177635bf506
fengchuang <feng.chuang@163..com>
2018-06-19 11:49:11 +0800

    打成一个包

---
+++ src/main/java/com/demo/Test.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+		List<String> list = Lists.newArrayList();
+		list.add("1");
+		list.add("2");
+		System.out.println(list);
+	}
+}
